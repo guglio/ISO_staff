252a43f9442d8631f7800b935a9f52aa056f709f
Guglielmo Turco <guglielmo.turco@gmail.com>
2017-03-06 12:05:42 +0100

    Order list, add new employees

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
+var urlDB = "http://127.0.0.1:5984";
+
 var app = angular.module('app', ['ngRoute'])
   .config(function ($routeProvider,$locationProvider) {
       $routeProvider.
-          when('/scheda/:id',
+          when('/ciam/:id',
             {
-              templateUrl: 'views/scheda.html'
+              templateUrl: 'views/scheda.html',
+              controller: 'SchedaCtrl'
+            }).
+          when('/new_dipendente',
+            {
+              templateUrl: 'views/dipendente.html',
+              controller: 'NewCtrl'
+            }).
+          when('/new_corso',
+            {
+              templateUrl: 'views/corso.html',
+              controller: 'NewCtrl'
             }).
           when('/',
             {
@@ -14,46 +27,43 @@ var app = angular.module('app', ['ngRoute'])
             redirectTo: '/'
           });
   })
-  .controller("SchedaCtrl", function($scope, $location, $routeParams){
-    //var accessData = window.localStorage['dipendenti'];
-    $scope.db = angular.fromJson(window.localStorage['dipendenti']);
-    $scope.dipendente = [];
-    cod_fiscale = $routeParams.id;
-
-    angular.forEach($scope.db, function(value, key){
-      if(value.cod_fiscale == cod_fiscale){
-        $scope.dipendente = value;
-      }
-    });
+  .controller("SchedaCtrl", function($scope, $http, $routeParams){
+    $http.get(urlDB+'/ciam/'+$routeParams.id)
+         .then(function(res){
+            $scope.dipendente = res.data;
+            if(!$scope.dipendente.corsi)
+              return;
+            else{
+            var corsi = "[";
+              angular.forEach($scope.dipendente.corsi, function(value, key){
+                corsi += "\""+value.id+"\""+",";
+              });
+              corsi = corsi.slice(0, -1);
+              corsi += "]";
+              $http.get(urlDB+'/corsi_formazione/_all_docs?keys='+corsi+"&include_docs=true").
+                then(function(res){
+                  $scope.corsi = res.data.rows;
+                });
+            }
+          });
   })
   .controller('AllCtrl', function($scope, $http) {
-    $http.get('data/dipendenti_test.json')
+    $http.get(urlDB+'/ciam/_design/all/_view/all')
          .then(function(res){
-            $scope.personale = res.data;
-            window.localStorage['dipendenti'] = angular.toJson($scope.personale);
+            $scope.personale = res.data.rows;
           });
-    $http.get('data/corsi_test.json')
-         .then(function(res){
-            window.localStorage['corsi'] = angular.toJson(res.data);
-          });
-  })
-  .controller('CorsiCtrl', function($scope,$routeParams){
 
-    $scope.corsidb = angular.fromJson(window.localStorage['corsi']);
-    $scope.corsi = [];
-    cod_fiscale = $routeParams.id;
-    angular.forEach($scope.corsidb, function(value, key){
-      var flagIn = 0;
-      var esitoCorso = "";
-      angular.forEach(value.partecipanti, function(v, k){
-        if(cod_fiscale == v.dipendente){
-          flagIn = 1;
-          esitoCorso = v.risultato;
-        }
-      });
-      if(flagIn){
-        value.esito = esitoCorso;
-        $scope.corsi.push(value);
-      }
-    });
+      $scope.orderProp = "value.cognome";
+      $scope.asc = false;
+
+  })
+  .controller('NewCtrl', function($scope, $http){
+    $scope.submitMyForm = function(){
+        /* while compiling form , angular created this object*/
+        $scope.fields.nome_completo = $scope.fields.cognome + " " + $scope.fields.nome;
+        var data = $scope.fields;
+        /* post to server*/
+        console.log(data);
+        $http.post(urlDB+'/ciam', data);
+    }
   });
